0dc23358ad43bf91b30fbbc7c742f294944ba63b
soldafon <medoviysup889@gmail.com>
2026-01-19 16:19:18 +0000

    scrapped

---
+++ tasklist.py
@@ -0,0 +1,6 @@
+tasklist = [input("Enter the first task:") , input("Enter the second task:") , input("Enter the third task:")]
+
+for (i, task) in enumerate(tasklist, start=1):
+    print(f"{i}. {task}")
+
+# nvm im scrapping that for now i have no ideas how to implement that
