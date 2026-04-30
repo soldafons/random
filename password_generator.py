85be3846b5542aa12d2d2295d70c293dc0023c65
soldafon <medoviysup889@gmail.com>
2026-04-30 15:20:31 +0000

    Updated

--- password_generator.py
+++ password_generator.py
@@ -1,27 +1,38 @@
-import random
+import secrets
 import string
 
+num = 0
+synum = ""
 strandom = ""
 passw = ""
-synum = ""
-num = 0
 
-while True:
-    try:
-        num = int(input("how long do you want your password to be"))
-        break
-    except ValueError:
-        print("enter an integer")
-        continue
-while synum not in ["yes", "no"]:
-        synum = input("do you want to include symbols?").lower()
-        print("choose from yes or no")
+def length(num):
+    while True:
+        try:   
+            num = int(input("how long do you want your password to be: "))
+            break
+        except ValueError:
+            print("enter an integer")
+            continue
+    return num
+num = length(num)
 
-if synum == "no":
-    strandom = string.ascii_letters + string.digits
-elif synum == "yes":
-    strandom = string.ascii_letters + string.digits + string.punctuation
+def choice(synum):
+    while synum not in ["yes" , "no"]:
+        synum = input("do you want to include symbols: ").lower()
+        if synum == "yes" or synum == "no":
+            return synum
+        else:
+            print("choose from yes or no")
+synum = choice(synum)
 
-for i in range(1, num+1):
-    passw += random.choice(strandom)
+def result(strandom):
+    if synum == "no":
+        strandom = string.ascii_letters + string.digits
+    else:
+        strandom = string.ascii_letters + string.digits + string.punctuation
+    return strandom
+strandom = result(strandom)
+
+passw += ''.join(secrets.choice(strandom) for i in range (num))
 print(passw)
